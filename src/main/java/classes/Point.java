b92b442bc80c7e453c97a1aa44f77f97c5e933d5
jbernal <j.bernal@upm.es>
2022-09-11 18:48:43 +0200

    fixed Point class

--- src/main/java/classes/Point.java
+++ src/main/java/classes/Point.java
@@ -1,4 +1,4 @@
-package lang;
+package classes;
 
 public class Point {
 
--- src/test/java/classes/PointTest.java
+++ src/test/java/classes/PointTest.java
@@ -1,6 +1,5 @@
 package classes;
 
-import lang.Point;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
